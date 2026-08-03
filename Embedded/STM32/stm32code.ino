97b4f6d101e19f47583ca5cc1895bdf85d9a8d4a
MUHAMMAD MAAZ <maaazmaazhee@gmail.com>
2026-08-03 07:32:15 +0500

    Implement terrain-based PWM control for output

--- Embedded/STM32/stm32code.ino
+++ Embedded/STM32/stm32code.ino
@@ -1,1 +1,51 @@
+// Inputs from Raspberry Pi
+const int asphaltPin = PA0;
+const int gravelPin = PA1;
+const int speedPin = PA2;
 
+// PWM Output
+const int pwmPin = PA8;
+
+void setup()
+{
+    pinMode(asphaltPin, INPUT_PULLDOWN);
+    pinMode(gravelPin, INPUT_PULLDOWN);
+    pinMode(speedPin, INPUT_PULLDOWN);
+
+    pinMode(pwmPin, PWM);
+
+    analogWriteResolution(8);   // 0-255
+}
+
+void loop()
+{
+    bool asphalt = digitalRead(asphaltPin);
+    bool gravel = digitalRead(gravelPin);
+    bool speed = digitalRead(speedPin);
+
+    if (asphalt)
+    {
+        // Lowest output
+        analogWrite(pwmPin, 0);
+    }
+
+    else if (gravel)
+    {
+        // Medium output (~3 A after calibration)
+        analogWrite(pwmPin, 128);
+    }
+
+    else if (speed)
+    {
+        // Maximum output
+        analogWrite(pwmPin, 255);
+    }
+
+    else
+    {
+        // No terrain selected
+        analogWrite(pwmPin, 0);
+    }
+
+    delay(5);
+}
